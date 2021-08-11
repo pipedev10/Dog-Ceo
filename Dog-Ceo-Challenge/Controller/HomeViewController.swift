2694a4509a379bd2a14d84a342e76d9d505f8f73
Felipe Carrasco <felipe@bakers.dev>
2021-08-10 21:35:28 -0400

    01 feature: create list breeds

--- Dog-Ceo-Challenge/Controller/HomeViewController.swift
+++ Dog-Ceo-Challenge/Controller/HomeViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    
     // MARK: - Properties
     var breed = [Breed]()
     let breedService: BreedService
+    let tableBreeds: UITableView = {
+        let table = UITableView()
+        table.register(BreedTableViewCell.self, forCellReuseIdentifier: BreedTableViewCell.identifier)
+        return table
+    }()
+    
     
     // MARK: - Initialization
     init(breedService: BreedService) {
@@ -29,19 +34,43 @@ class HomeViewController: UIViewController {
         view.backgroundColor = .blue
         
         breedService.getListBreeds{ [weak self] breedResult in
-            
             switch breedResult {
             case .failure(let error):
                 //ErrorPresenter.showError(message: "There was an error getting the acronyms", on: self)
                 print("error: \(error) ")
             case .success(let breed):
                 self?.breed = breed
-                //self.tableView.reloadData()
+                self?.tableBreeds.reloadData()
                 
             }
         }
         
-       
+        tableBreeds.dataSource = self
+        tableBreeds.delegate = self
+        
+        view.addSubview(tableBreeds)
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableBreeds.frame = view.bounds
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return breed.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableBreeds.dequeueReusableCell(withIdentifier: BreedTableViewCell.identifier, for: indexPath) as? BreedTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.configure(text: breed[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
 }
